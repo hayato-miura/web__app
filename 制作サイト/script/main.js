c33216cc9b550cc444d801c3a178f3445b95977f
hayato-miura <miura.miura.515@gmail.com>
2023-03-09 15:47:53 +0900

    微調整をしました

--- 制作サイト/script/main.js
+++ 制作サイト/script/main.js
@@ -3,13 +3,12 @@
 // 学習時間の棒グラフ
 
 var barChartData = {
-    labels: ['9/2','9/3','9/4','9/5','9/6','9/7','9/8',
-             '9/9','9/10','9/11','9/12','9/13','9/14'],
+    labels: ['2','4','6','8','10','12','14','16','18','20','22','24','26','28','30','32'],
     datasets: [
       {
         label: 'blue',
         data: ['0.2','0.3','0.11','0.5','0.2','0.5','0.4',
-             '0.17','0.3','0.7','0.3','0.6','0.4'],
+             '0.17','0.3','0.7','0.3','0.6','0.4',],
         borderColor : "rgba(54,164,235,0.8)",
         backgroundColor : "rgba(54,164,235,0.5)",
       },
@@ -35,13 +34,13 @@ var barChartData = {
   var myDoughnutChart1= new Chart(ctx, {
     type: 'doughnut',
     data: {
-      labels: ["賛成", "反対", "わからない", "未回答"], //データ項目のラベル
+      labels: ["JavaScript", "CSS", "PHP", "HTML"], //データ項目のラベル
       datasets: [{
           backgroundColor: [
-              "#c97586",
-              "#bbbcde",
-              "#93b881",
-              "#e6b422"
+              "#0345ec",
+              "#0f71bd",
+              "#20bdde",
+              "#3cccefe"
           ],
           data: [45, 32, 18, 5] //グラフのデータ
       }]
@@ -50,7 +49,6 @@ var barChartData = {
       title: {
         display: true,
         //グラフタイトル
-        text: '新法案賛否'
       }
     }
   });
@@ -62,10 +60,10 @@ var barChartData = {
       labels: ["賛成", "反対", "わからない", "未回答"], //データ項目のラベル
       datasets: [{
           backgroundColor: [
-              "#c97586",
-              "#bbbcde",
-              "#93b881",
-              "#e6b422"
+            "#0345ec",
+            "#0f71bd",
+            "#20bdde",
+            "#3cccefe"
           ],
           data: [45, 32, 18, 5] //グラフのデータ
       }]
